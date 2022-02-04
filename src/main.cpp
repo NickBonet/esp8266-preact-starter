1d838c1963bb28205256909b2e1ae3064cef5e18
NickBonet <nick.b98@Hotmail.com>
2022-02-04 17:28:13 -0500

    Move web UI files to /data/www/

--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,21 @@
 
 AsyncWebServer server(9080);
 
-const char* ssid = WIFI_SSID;
-const char* password = WIFI_PASSWORD;
+const char *ssid = WIFI_SSID;
+const char *password = WIFI_PASSWORD;
 
-void notFound(AsyncWebServerRequest *request) {
+void notFound(AsyncWebServerRequest *request)
+{
     request->send(404, "text/plain", "Not found");
 }
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
 
     // LittleFS initialization
-    if(!LittleFS.begin()){
+    if (!LittleFS.begin())
+    {
         Serial.println("An error has occurred while mounting LittleFS filesystem.");
         return;
     }
@@ -31,7 +34,8 @@ void setup() {
     // WiFi initialization
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    if (WiFi.waitForConnectResult() != WL_CONNECTED)
+    {
         Serial.printf("WiFi connection failed!\n");
         return;
     }
@@ -39,16 +43,16 @@ void setup() {
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
 
-    server.serveStatic("/", LittleFS, "/");
+    server.serveStatic("/", LittleFS, "/www/");
 
-    server.on("*", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(LittleFS, "/index.html");
-    });
+    server.on("*", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(LittleFS, "/www/index.html"); });
 
     server.onNotFound(notFound);
     server.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    // put your main code here, to run repeatedly:
 }
